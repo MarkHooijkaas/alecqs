6e6fba471a7d623b9688a9c58b23a687f442182d
Mark Hooijkaas <mark@hooijkaas.net>
2017-01-30 01:19:48 +0100

    Added Binding of parser variables to Javascript evaluation

--- src/main/java/org/kisst/alecqs/command/EvalExpression.java
+++ src/main/java/org/kisst/alecqs/command/EvalExpression.java
@@ -2,9 +2,13 @@ package org.kisst.alecqs.command;
 
 import org.kisst.alecqs.Parser;
 
+import javax.script.Bindings;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 public class EvalExpression {
 
@@ -18,7 +22,76 @@ public class EvalExpression {
 		}
 
 		try {
-			return engine.eval(expr);
+			return engine.eval(expr, new ParserBinding(parser));
 		} catch (ScriptException e) { throw new RuntimeException(e); }
+	}
+
+	private static class ParserBinding implements Bindings {
+		private final Parser parser;
+
+		private ParserBinding(Parser parser) {
+			this.parser = parser;
+		}
+
+		@Override
+		public Object put(String s, Object o) {
+			//throw new RuntimeException("Not Implemented: put("+s+","+o+")");
+			return o;
+		}
+
+		@Override
+		public void putAll(Map<? extends String, ?> map) {
+			throw new RuntimeException("Not Implemented");
+		}
+
+		@Override
+		public void clear() {
+			throw new RuntimeException("Not Implemented");
+		}
+
+		@Override
+		public Set<String> keySet() {
+			throw new RuntimeException("Not Implemented");
+		}
+
+		@Override
+		public Collection<Object> values() {
+			throw new RuntimeException("Not Implemented");
+		}
+
+		@Override
+		public Set<Entry<String, Object>> entrySet() {
+			throw new RuntimeException("Not Implemented");
+		}
+
+		@Override
+		public int size() {
+			throw new RuntimeException("Not Implemented");
+		}
+
+		@Override
+		public boolean isEmpty() {
+			throw new RuntimeException("Not Implemented");
+		}
+
+		@Override
+		public boolean containsKey(Object o) {
+			return parser.getProp(o.toString())!=null;
+		}
+
+		@Override
+		public boolean containsValue(Object o) {
+			throw new RuntimeException("Not Implemented");
+		}
+
+		@Override
+		public Object get(Object o) {
+			return parser.getProp(o.toString());
+		}
+
+		@Override
+		public Object remove(Object o) {
+			throw new RuntimeException("Not Implemented");
+		}
 	}
 }
